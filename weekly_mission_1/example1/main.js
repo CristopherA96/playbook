3b86a49bd0ce443e37b43db0e52cff85276fb299
Cristopher AI <cristopher.pl451@gmail.com>
2022-04-17 00:27:56 -0500

    Updating main.js in weekly_mission_1/example1

--- weekly_mission_1/example1/main.js
+++ weekly_mission_1/example1/main.js
@@ -5,3 +5,16 @@ console.log("What's up explorer from Launch X")
 // Example 1
 let myCar = new Object()    // empty object
 myCar.model = "Audi"
+
+
+const myModule = (() => {
+    const privateFoo = "This is private"
+
+    const exported = {
+        publicFoo : "This is public"
+
+    }
+    return exported
+})
+
+console.log(myModule.privateFoo)
