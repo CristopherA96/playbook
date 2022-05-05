e491d86c9070447e6963451cacc821395eac18f9
Cristopher AI <cristopher.pl451@gmail.com>
2022-05-05 13:56:49 -0500

    Adding the main.js file in example 6

---
+++ weekly_mission_1/example6/main.js
@@ -0,0 +1,22 @@
+/************************************************
+ * Launch X - Backend with Node JS
+ * 
+ * Example 6 "Class modification"
+ * 
+ * 
+ * Author: Carlo Gilmar
+ * Editor: Cristopher Paredes 
+ ************************************************/
+// main.js file in example 6
+
+// Invoking the module that modifies the instantiated object in the patcher.js file
+require('./patcher');
+
+// You can now directly use the object instantiated in the logger.js module
+// it will give you the modified object
+// const dLogger = new logger.constructor('DEFAULT'); 
+console.log('Sending message for DEFAULT object instantiation\n');
+const logger = require('./logger');
+
+// Calling the method customMessage for the logger object
+logger.customMessage()
